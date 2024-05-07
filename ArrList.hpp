3ec8f11eb9a75861e45b9712f2bfa09e9c019545
272572PPiwnicki <135513463+272572PPiwnicki@users.noreply.github.com>
2024-05-07 14:20:51 +0200

    Update ArrList.hpp

--- ArrList.hpp
+++ ArrList.hpp
@@ -1,13 +1,16 @@
 #pragma once
 #include <iostream>
+#include "Structure.hpp"
 #include "Node.hpp"
+
 using namespace std;
 
-class ArrList {
+class ArrList : public Structure
+{
 private:
-    Node* array; //Tablica przechowuj¹ca elementy
+    Node* array; //Tablica przechowujaca elementy
     int size; // Aktualny rozmiar kolejki
-    int capacity; // Pojemnoœæ tablicy
+    int capacity; // Pojemnosc tablicy
 
 public:
     ArrList(int capacity);
@@ -15,8 +18,8 @@ public:
 
     void insert(int data, int priority);
     int extractMax();
-    int peek() const;
+    int peek();
     void modifyKey(int data, int newPriority);
     int getSize();
-
+    virtual void generateRandomNumbers(int count) override;
 };
